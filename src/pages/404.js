d151f315bde697134fca4ab79d88e3a8f72c63b6
Jim Tyhurst <jim@tyhurst.com>
2021-07-14 10:40:46 -0700

    Customized 404 error page.

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,8 +3,13 @@ import Layout from '../components/layout/Layout';
 
 const NotFound = () => (
   <Layout>
-    <h1>Not Found</h1>
-    <p>You just hit a page that doesn&#39;t exist...</p>
+    <section className="py-20 lg:pb-20 lg:pt-24">
+      <div className="px-6 mx-auto lg:w-2/3 text-center">
+        <h1>404</h1>
+        <h2>Not Found</h2>
+        <p>The page you are looking for may have been moved or deleted. Try searching again.</p>
+      </div>
+    </section>
   </Layout>
 );
 
