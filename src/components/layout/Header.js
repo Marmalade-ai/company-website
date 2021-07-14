36899752f445ca6c6429489714497790d57b9dbc
Jim Tyhurst <jim@tyhurst.com>
2021-07-14 13:36:15 -0700

    Refactored to use semantic HTML where possible.

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,43 +1,43 @@
 import React from 'react';
-import AnchorLink from 'react-anchor-link-smooth-scroll';
 import { Link } from 'gatsby';
-import LogoIcon from '../../svg/LogoIcon';
-import Button from '../Button';
 import { StaticImage } from 'gatsby-plugin-image';
+import Button from '../Button';
 
 const Header = () => (
   <header className="sticky top-0 bg-white shadow z-50">
-    <div className="container flex flex-col sm:flex-row justify-between items-center mx-auto py-4 px-8">
+    <nav className="container flex flex-col sm:flex-row justify-between items-center mx-auto py-4 px-8">
       <div className="flex items-center text-2xl">
         <div className="w-12 mr-3">
-          <StaticImage src="../../images/marmalade-logo.jpg" alt="Marmalade AI Logo" />
+          <Link to="/#">
+            <StaticImage src="../../images/marmalade-logo.jpg" alt="Marmalade AI Logo" />
+          </Link>
         </div>
         Marmalade AI
       </div>
       <div className="flex mt-4 sm:mt-0">
-        <Link className="px-4" to="../../#">
+        <Link className="px-4" to="/#">
           Home
         </Link>
-        <Link className="px-4" to="../../#benefits">
+        <Link className="px-4" to="/#benefits">
           Features
         </Link>
-        <Link className="px-4" to="../../#pricing">
+        <Link className="px-4" to="/#pricing">
           Pricing
         </Link>
-        <Link className="px-4" to="../../#">
+        <Link className="px-4" to="/#">
           News
         </Link>
-        <Link className="px-4" to="../../#">
+        <Link className="px-4" to="/#">
           FAQ
         </Link>
-        <Link className="px-4" to="../../about/">
+        <Link className="px-4" to="/about">
           About Us
         </Link>
       </div>
       <div className="hidden md:block">
         <Button className="text-sm">Login</Button>
       </div>
-    </div>
+    </nav>
   </header>
 );
 
--- src/pages/privacy-policy.js
+++ src/pages/privacy-policy.js
@@ -3,20 +3,23 @@ import Layout from '../components/layout/Layout';
 
 const Index = () => (
   <Layout>
-    <section className="py-20 lg:pb-20 lg:pt-24">
+    <article className="py-20 lg:pb-20 lg:pt-24">
       <div className="px-6 mx-auto lg:w-2/3">
         <h1>Privacy Policy</h1>
-        <p>Last Updated: July 8, 2021</p>
+        <p>
+          Last Updated: <time datetime="2021-07-14 11:42">July 14, 2021</time>
+        </p>
         <p>
           Marmalade AI, Inc. has created this Privacy Notice to explain why we collect particular
-          information and how we will protect your personal privacy when you visit our websites, or
+          information and how we will protect your personal privacy when you visit our websites or
           otherwise engage with Marmalade AI (e.g., web browsing, online chatting, and online
           messaging) including through our company website and the Marmalade AI web application.
         </p>
         <p>
-          The following discloses our information collection, use, storage and other data processing
-          practices for Marmalade AI.
+          The following discloses our information collection, use, storage, and other data
+          processing practices for Marmalade AI.
         </p>
+
         <h2>Collection of Personal Information</h2>
         <p>
           Personal information is any information that personally identifies you or from which you
@@ -29,6 +32,7 @@ const Index = () => (
           correspondence with Marmalade AI representatives, or when you complete an online
           application or inquiry form.
         </p>
+
         <h2>We us various technologies to collect information</h2>
         <h3>Cookies</h3>
         <p>
@@ -44,9 +48,9 @@ const Index = () => (
           customized, personalized content. In some case we may use your IP address to customize
           content based on your location.
         </p>
+
         <h2>Third Parties</h2>
         <p>We may disclose your information to third parties as follows:</p>
-
         <h3>Third-Party Service Providers and Partners</h3>
         <p>
           At times Marmalade AI will use third parties to process your information on our behalf,
@@ -63,7 +67,7 @@ const Index = () => (
           We may seek your consent to disclose your information to third parties if we are required
           to do so. Where we have sought, and you have provided, your express consent for a
           particular purpose, please note you have the right to withdraw your consent at any time by
-          notifying us at the contact information below.
+          notifying us through the contact information shown below.
         </p>
         <h3>De-Identified and Aggregate Information</h3>
         <p>
@@ -78,12 +82,12 @@ const Index = () => (
         </p>
         <p>
           Occasionally, we may contract with a third party to communicate on our behalf to the
-          third-party's contacts. We don’t collect the email addresses or contact information from
-          these third parties, and we don’t have access to their mailing lists.
+          third-party's contacts. We do not collect the email addresses or contact information from
+          these third parties, and we do not have access to their mailing lists.
         </p>
         <p>
           We may provide third party mailers with a suppression list of contacts to exclude from
-          their email distribution list. In this situation, the third party doesn’t have permission
+          their email distribution list. In this situation, the third party does not have permission
           to keep or market to contacts contained in these suppression lists, or to use them in any
           way other than as a suppression list for a mailing they are providing on our behalf.
         </p>
@@ -93,24 +97,22 @@ const Index = () => (
         </p>
 
         <h2>Children's Privacy</h2>
-
         <p>
           Marmalade AI does not knowingly collect information from children as defined by local law,
           and does not target its websites or mobile applications to children under these ages. We
-          encourage parents and guardians to take an active role in their children’s online and
+          encourage parents and guardians to take an active role in their children's online and
           mobile activities and interests.
         </p>
 
         <h2>Data Retention</h2>
-
         <p>
           The need to retain data varies depending on the type of data. Marmalade AI will retain
           your personal data as long as necessary for the purpose of processing (e.g., archival,
           data analysis). We may also retain and use your information in order to comply with our
           legal obligations, resolve disputes, prevent abuse, and enforce our agreements.
         </p>
-        <h2>Security</h2>
 
+        <h2>Security</h2>
         <p>
           Marmalade AI takes seriously the trust you place in us. All information provided to
           Marmalade AI is transmitted using SSL (Secure Socket Layer) encryption. SSL is a proven
@@ -125,19 +127,22 @@ const Index = () => (
           the security of such information. We strongly advise you not to share your password with
           anyone.
         </p>
+
         <h2>Changes to the Privacy Notice</h2>
         <p>
           Changes may be made to this Privacy Notice and personal information may be used for new
           purposes. When significant changes are made to our privacy practices, they will be
-          disclosed here. For your convenience please refer to the last updated date.
+          disclosed here. For your convenience please refer to the last updated date displayed on
+          this page.
         </p>
+
         <h2>Contact Information</h2>
         <p>
           If you have any questions about this Privacy Notice, the practices of this site, or your
-          dealings with this site, you can send email to <em>privacy@marmalade.ai</em>.
+          dealings with this site, you can send email to <i>privacy@marmalade.ai</i>.
         </p>
       </div>
-    </section>
+    </article>
   </Layout>
 );
 
