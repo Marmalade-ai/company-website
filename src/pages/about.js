0ae14a161e0f4cc7b36c51da4f458e8178d72891
Jim Tyhurst <jim@tyhurst.com>
2021-07-15 09:13:09 -0700

    Added biographies.

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,50 +10,50 @@ import customerData from '../data/customer-data';
 import HeroImage from '../svg/HeroImage';
 import SvgCharts from '../svg/SvgCharts';
 
-const Index = () => (
+const About = () => (
   <Layout>
-
     <section id="features" className="py-20 lg:pb-20 lg:pt-12">
       <div className="container mx-auto text-center">
-        <h1 className="text-4xl lg:text-5xl xl:text-6xl font-semibold leading-none">
-          Our Team
-          </h1>
-        <p className="text-xl lg:text-2xl mt-6 font-light">
-          It's very big and it's very small!
-          </p>
+        <h1 className="text-4xl lg:text-5xl xl:text-6xl font-semibold leading-none">Our Team</h1>
+        <p className="text-xl lg:text-2xl mt-6 font-light">It's very big and it's very small!</p>
         <div className="grid grid-cols-2 sm:flex-row sm:-mx-3 mt-12">
           <div className="flex-1 px-3">
             <Card className="mb-8">
               <p className="font-semibold text-xl">Craig Fisk</p>
+              <p className="font-semibold text-lg">CEO</p>
               <p className="mt-4">
-                There’s a founding team of four, all remote. As for me: University of Chicago Booth MBA, Intel software product line manager, co-founder/CEO of Series A venture mobile CRM company. That was then, this is now. Networking is broken. We’re starting with virtual peer networking among freelancers. We’re writing the front end in Vue.js and the back end in Google Cloud Functions with NLP and Firestore. The future is software driven by users.
+                Series A mobile CRM, new software products/channels at Intel. Chicago Booth MBA. San
+                Jose, CA.
               </p>
             </Card>
           </div>
           <div className="flex-1 px-3">
             <Card className="mb-8">
               <p className="font-semibold text-xl">Jim Tyhurst</p>
+              <p className="font-semibold text-lg">CTO</p>
               <p className="mt-4">
-                An enim nullam tempor gravida donec enim ipsum blandit porta justo integer odio
-                velna vitae auctor integer.
-              </p>
-            </Card>
-          </div>
-          <div className="flex-1 px-3">
-            <Card className="mb-8">
-              <p className="font-semibold text-xl">Michael Porto</p>
-              <p className="mt-4">
-                An enim nullam tempor gravida donec enim ipsum blandit porta justo integer odio
-                velna vitae auctor integer.
+                Software architect, Machine Learning / Natural Language Processing (ML/NLP),
+                independent consultant. UCLA PhD Linguistics. Portland, OR.
               </p>
             </Card>
           </div>
           <div className="flex-1 px-3">
             <Card className="mb-8">
               <p className="font-semibold text-xl">Vivian Tran</p>
+              <p className="font-semibold text-lg">Marketing Advisor</p>
               <p className="mt-4">
-                An enim nullam tempor gravida donec enim ipsum blandit porta justo integer odio
-                velna vitae auctor integer.
+                New segment marketing, healthcare industry marketing manager. UC Berkeley. Berkeley,
+                CA.
+              </p>
+            </Card>
+          </div>
+          <div className="flex-1 px-3">
+            <Card className="mb-8">
+              <p className="font-semibold text-xl">Michael Porto</p>
+              <p className="font-semibold text-lg">Product Management Advisor</p>
+              <p className="mt-4">
+                Big data, mobile, ML, Intel, Wells Fargo, Ernst & Young. Princeton MS CS. Charlotte,
+                NC.
               </p>
             </Card>
           </div>
@@ -63,12 +63,7 @@ const Index = () => (
         </p>
       </div>
     </section>
-
-
-
-
-
   </Layout>
 );
 
-export default Index;
+export default About;
