96e72fd74c7531e9d3cab88a53b632a3f579c2b3
Jim Tyhurst <jim@tyhurst.com>
2021-07-15 11:50:47 -0700

    Added photos.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,60 +1,72 @@
 import React from 'react';
 import Button from '../components/Button';
 import Card from '../components/Card';
-import CustomerCard from '../components/CustomerCard';
-import LabelText from '../components/LabelText';
 import Layout from '../components/layout/Layout';
-import SplitSection from '../components/SplitSection';
-import StatsBox from '../components/StatsBox';
-import customerData from '../data/customer-data';
-import HeroImage from '../svg/HeroImage';
-import SvgCharts from '../svg/SvgCharts';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const About = () => (
   <Layout>
     <section id="features" className="py-20 lg:pb-20 lg:pt-12">
       <div className="container mx-auto text-center">
-        <h1 className="text-4xl lg:text-5xl xl:text-6xl font-semibold leading-none">Our Team</h1>
-        <p className="text-xl lg:text-2xl mt-6 font-light">It's very big and it's very small!</p>
+        <h1 className="text-4xl lg:text-5xl xl:text-6xl font-semibold leading-none">Founders</h1>
         <div className="grid grid-cols-2 sm:flex-row sm:-mx-3 mt-12">
           <div className="flex-1 px-3">
             <Card className="mb-8">
-              <p className="font-semibold text-xl">Craig Fisk</p>
+              <StaticImage
+                className="rounded-lg shadow-lg	"
+                src="../images/craig_fisk_headshot.jpg"
+                alt="Photo of Craig Fisk."
+              />
+              <p className="mt-4 font-semibold text-xl">Craig Fisk</p>
               <p className="font-semibold text-lg">CEO</p>
-              <p className="mt-4">
-                Series A mobile CRM, new software products/channels at Intel. Chicago Booth MBA. San
-                Jose, CA.
-              </p>
+              <p className="mt-4">Series A mobile CRM, new software products/channels at Intel.</p>
+              <p>Chicago Booth MBA.</p>
+              <p>San Jose, CA.</p>
             </Card>
           </div>
           <div className="flex-1 px-3">
             <Card className="mb-8">
-              <p className="font-semibold text-xl">Jim Tyhurst</p>
+              <StaticImage
+                className="rounded-lg shadow-lg	"
+                src="../images/jim_tyhurst_headshot.jpg"
+                alt="Photo of Jim Tyhurst."
+              />
+              <p className="mt-4 font-semibold text-xl">Jim Tyhurst</p>
               <p className="font-semibold text-lg">CTO</p>
               <p className="mt-4">
                 Software architect, Machine Learning / Natural Language Processing (ML/NLP),
-                independent consultant. UCLA PhD Linguistics. Portland, OR.
+                independent consultant.
               </p>
+              <p>UCLA PhD Linguistics.</p>
+              <p>Portland, OR.</p>
             </Card>
           </div>
           <div className="flex-1 px-3">
             <Card className="mb-8">
-              <p className="font-semibold text-xl">Vivian Tran</p>
+              <StaticImage
+                className="rounded-lg shadow-lg	"
+                src="../images/vivian_tran_headshot.jpg"
+                alt="Photo of Vivian Tran."
+              />
+              <p className="mt-4 font-semibold text-xl">Vivian Tran</p>
               <p className="font-semibold text-lg">Marketing Advisor</p>
-              <p className="mt-4">
-                New segment marketing, healthcare industry marketing manager. UC Berkeley. Berkeley,
-                CA.
-              </p>
+              <p className="mt-4">New segment marketing, healthcare industry marketing manager.</p>
+              <p>UC Berkeley.</p>
+              <p>Berkeley, CA.</p>
             </Card>
           </div>
           <div className="flex-1 px-3">
             <Card className="mb-8">
-              <p className="font-semibold text-xl">Michael Porto</p>
+              <StaticImage
+                className="rounded-lg shadow-lg	"
+                src="../images/michael_porto_headshot.jpg"
+                alt="Photo of Michael Porto."
+              />
+              <p className="mt-4 font-semibold text-xl">Michael Porto</p>
               <p className="font-semibold text-lg">Product Management Advisor</p>
-              <p className="mt-4">
-                Big data, mobile, ML, Intel, Wells Fargo, Ernst & Young. Princeton MS CS. Charlotte,
-                NC.
-              </p>
+              <p className="mt-4">Big data, mobile, ML, Intel, Wells Fargo, Ernst & Young.</p>
+              <p>Princeton MS CS.</p>
+              <p>Charlotte, NC.</p>
             </Card>
           </div>
         </div>
