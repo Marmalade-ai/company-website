1fa995676d8832b40ef74243dc21771dc6b8d2be
Jim Tyhurst <jim@tyhurst.com>
2021-08-07 19:04:48 -0700

    Added photos to 'Digital Marketing' community.

--- src/pages/communities/digital-marketing.js
+++ src/pages/communities/digital-marketing.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
 import Button from '../../components/Button';
 import Layout from '../../components/layout/Layout';
 import SEO from '../../components/seo';
+import SplitSection from '../../components/SplitSection';
 
 const DigitalMarketing = () => (
   <Layout>
@@ -14,6 +16,39 @@ const DigitalMarketing = () => (
     </div>
     <article className="py-20 lg:pb-4 lg:pt-4">
       <div className="px-6 mx-auto lg:w-2/3">
+        <SplitSection
+          reverseOrder
+          primarySlot={
+            <div className="lg:pl-32 xl:pl-48">
+              <p className="splitBody">
+                Digital marketing means collaboration to get feedback.
+              </p>
+            </div>
+          }
+          secondarySlot={
+            <StaticImage
+              className="rounded-lg shadow-lg"
+              src="../../images/kobu-agency-7okkFhxrxNw-unsplash_300.jpg"
+              alt="Digital marketing means collaboration to get feedback."
+            />
+          }
+        />
+        <SplitSection
+          primarySlot={
+            <div className="lg:pr-32 xl:pr-48">
+              <p className="splitBody">
+                Search Engine Optimization (SEO) is key to inbound marketing.
+              </p>
+            </div>
+          }
+          secondarySlot={
+            <StaticImage
+              className="rounded-lg shadow-lg	"
+              src="../../images/pexels-freeboilergrants-7119258_300.jpg"
+              alt="Search Engine Optimization (SEO) is key to inbound marketing."
+            />
+          }
+        />
         <p>
           Freelancers in digital marketing specialize in areas such as SEO,
           content, new market development, website development, positioning,
