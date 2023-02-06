61d21a5e2ee1b6bc29c9b6e0686e86b130c9a13e
craigfisk <craigfisk@users.noreply.github.com>
2023-02-05 23:50:40 -0600

    Updated "copyright" notices to 2023.

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => (
         <h2 className="text-lg font-semibold">Marmalade AI, Inc.</h2>
         <ul className="mt-4 leading-loose list-none">
           <li>Virtual networking.</li>
-          <li>Copyright © 2021-2022 Marmalade AI, Inc.</li>
+          <li>Copyright © 2021-2023</li>
         </ul>
       </section>
       <section className="flex-1 px-3">
